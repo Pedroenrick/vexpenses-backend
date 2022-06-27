69fb8c7270a075dc1e4ae10971e8af5de92012db
Pedroenrick Felizardo <Pedro_penna22@hotmail.com>
2022-06-27 04:38:58 -0300

    Removendo informações de usuarios do login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,8 +19,7 @@ class AuthController extends Controller
         $credentials = $request->only('email', 'password');
 
         $token = auth('api')->attempt($credentials);
-        $user = auth('api')->user();
-
+    
         if (!$token) {
             return response()->json([
                 'message' => 'Invalid credentials'
@@ -29,7 +28,6 @@ class AuthController extends Controller
 
         return response()->json([
             'token' =>  $token,
-            'user' => $user
         ]);
     }
 
